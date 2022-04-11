6cc1197d7bd304def389f9819f7bf15342407d91
Lucas <lucas.alves@dp6.com.br>
2022-04-11 17:00:46 -0300

    Intial commit

---
+++ js/tagueamento.js
@@ -0,0 +1,22 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+
+  /*(function(){
+    easyCollect.safeFn('Custom HTML - menu', function(collect){
+      console.log('teste tag menu');
+      //collect.pageview('teste')
+
+      collect.delegate('mousedown', '.menu-lista-contato', function(collect){
+        collect.event('menu', 'entre_em_contato', 'link_externo');
+      });
+      collect.delegate('mousedown', '.menu-lista-download', function(collect){
+        collect.event('menu', 'download_pdf', 'download_pdf');
+      });
+     
+    });
+  })()*/
